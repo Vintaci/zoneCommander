d79b3841b70a62ecc45cc83177f7e22c3e4e4b59
Gold John King <GoldJohnKing@Live.cn>
2022-10-03 20:17:47 +0800

    Optimize server restart hint

--- GoldenScripts/zoneSetup.lua
+++ GoldenScripts/zoneSetup.lua
@@ -679,8 +679,13 @@ local missionCompleteCheck = function(event, sender)
 	end
 
 	if done then
-		trigger.action.outText("敌军已被彻底击败！任务完成！ \n\n服务器将于90秒后重启！", 90)
+		trigger.action.outText("敌军已被彻底击败！任务完成！ \n\n服务器将于90秒后清档重启！", 90)
 		mist.removeFunction(missionCompleteCheckSheduler)
+
+		mist.scheduleFunction(function(event, sender)
+			trigger.action.outText("服务器即将清档重启！", 60)
+		end, {}, timer.getTime() + 75)
+
 		mist.scheduleFunction(function(event, sender)
 			os.remove(filepath)
 			trigger.action.setUserFlag("TriggerFlagMissionComplete", true)
@@ -703,15 +708,19 @@ local restartHintTime = { 60, 180, 300, 900 }
 
 for key, value in pairs(restartHintTime) do -- Restart hint
 	mist.scheduleFunction(function(event, sender)
-		trigger.action.outText("服务器将于" .. value / 60 .. "分钟后定时重启！", 60)
+		trigger.action.outText("服务器将于" .. value / 60 .. "分钟后定时重启！", 90)
 	end, {}, timer.getTime() + restartTime - value)
 end
+
+mist.scheduleFunction(function(event, sender)
+	trigger.action.outText("服务器即将定时重启！", 60)
+end, {}, timer.getTime() + restartTime - 15)
 
 mist.scheduleFunction(function(event, sender) -- Restart
 	bc:update()
 	bc:saveToDisk()
 	trigger.action.setUserFlag("TriggerFlagScheduledRestart", true)
-end, {}, timer.getTime() + restartTime + 15) -- Wait 15 more seconds to avoid conflict with scheduled auto save
+end, {}, timer.getTime() + restartTime)
 
 -- Scheduled Restart Done
 
