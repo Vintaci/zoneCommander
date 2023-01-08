80fbdc02f26f0e08f590b1408e881afcdd00205d
Gold John King <GoldJohnKing@Live.cn>
2023-01-08 14:59:14 +0800

    Revert "Delay one second to solve mission progress load fail issue"
    This reverts commit 6f0133481dc26346accff0dfba617eeafa6fb220.

--- GoldenMission/l10n/DEFAULT/Load_Scripts.lua
+++ GoldenMission/l10n/DEFAULT/Load_Scripts.lua
@@ -28,8 +28,4 @@ else
     env.info("Foothold - LFS not available, using default script load path: " .. script_path)
 end
 
-local function load_scripts_scheduled(params)
-    load_scripts(params[1], params[2])
-end
-
-timer.scheduleFunction(load_scripts_scheduled, { script_path, script_list }, 1)
+load_scripts(script_path, script_list)
