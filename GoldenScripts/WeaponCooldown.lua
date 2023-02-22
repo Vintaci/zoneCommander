a4d6a327b15a4304c952aea7c91cf9bc1676a818
Gold John King <GoldJohnKing@Live.cn>
2023-02-22 11:04:49 +0800

    Fix weapon cooldown function not working on air units

--- GoldenScripts/WeaponCooldown.lua
+++ GoldenScripts/WeaponCooldown.lua
@@ -1,38 +1,38 @@
-local timer = timer
-local Group = Group
-local mist = mist
-
-local function ChangeGroupROE(groupName, optionIndex, roe)
+local function StopFiring(groupName, groupType)
     local group = Group.getByName(groupName)
 
-    if (nil == group) then
-        env.info("ChangeGroupROE: group " .. groupName .. " is nil")
+    if nil == group then
         return
     end
 
-    group:getController():setOption(optionIndex, roe)
-    env.info("ChangeGroupROE: set group " .. groupName .. " ROE(" .. optionIndex .. ") to " .. roe)
+    if "Air" == groupType then
+        group:getController():setOption(AI.Option.Air.id.ROE, AI.Option.Air.val.ROE.WEAPON_HOLD)
+        group:getController():setOption(AI.Option.Air.id.REACTION_ON_THREAT, AI.Option.Air.val.REACTION_ON_THREAT.NO_REACTION)
+    elseif "Ground" == groupType then
+        group:getController():setOption(AI.Option.Ground.id.ROE, AI.Option.Ground.val.ROE.WEAPON_HOLD)
+    elseif "Naval" == groupType then
+        group:getController():setOption(AI.Option.Naval.id.ROE, AI.Option.Naval.val.ROE.WEAPON_HOLD)
+    end
+end
+
+local function StartFiring(groupName, groupType)
+    local group = Group.getByName(groupName)
+
+    if nil == group then
+        return
+    end
+
+    if "Air" == groupType then
+        group:getController():setOption(AI.Option.Air.id.ROE, AI.Option.Air.val.ROE.WEAPON_FREE)
+        group:getController():setOption(AI.Option.Air.id.REACTION_ON_THREAT, AI.Option.Air.val.REACTION_ON_THREAT.EVADE_FIRE)
+    elseif "Ground" == groupType then
+        group:getController():setOption(AI.Option.Ground.id.ROE, AI.Option.Ground.val.ROE.OPEN_FIRE)
+    elseif "Naval" == groupType then
+        group:getController():setOption(AI.Option.Naval.id.ROE, AI.Option.Naval.val.ROE.OPEN_FIRE)
+    end
 end
 
 function WeaponCooldown(groupName, groupType, delay)
-    -- Air
-    if ("Air" == groupType) then
-        ChangeGroupROE(groupName, AI.Option.Air.id.ROE, AI.Option.Air.val.ROE.WEAPON_HOLD)
-        mist.scheduleFunction(ChangeGroupROE, { groupName, AI.Option.Air.id.ROE, AI.Option.Air.val.ROE.WEAPON_FREE }, timer.getTime() + delay)
-        return
-    end
-
-    -- Ground
-    if ("Ground" == groupType) then
-        ChangeGroupROE(groupName, AI.Option.Ground.id.ROE, AI.Option.Ground.val.ROE.WEAPON_HOLD)
-        mist.scheduleFunction(ChangeGroupROE, { groupName, AI.Option.Ground.id.ROE, AI.Option.Ground.val.ROE.OPEN_FIRE }, timer.getTime() + delay)
-        return
-    end
-
-    -- Naval
-    if ("Naval" == groupType) then
-        ChangeGroupROE(groupName, AI.Option.Naval.id.ROE, AI.Option.Naval.val.ROE.WEAPON_HOLD)
-        mist.scheduleFunction(ChangeGroupROE, { groupName, AI.Option.Naval.id.ROE, AI.Option.Naval.val.ROE.OPEN_FIRE }, timer.getTime() + delay)
-        return
-    end
+    mist.scheduleFunction(StopFiring, { groupName, groupType }, timer.getTime() + 3)
+    mist.scheduleFunction(StartFiring, { groupName, groupType }, timer.getTime() + delay)
 end
