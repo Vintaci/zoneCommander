ab01145d4590c24f670adb991e5798554ff94eeb
Gold John King <GoldJohnKing@Live.cn>
2023-08-04 10:30:35 +0800

    Refactor restart script

--- GoldenScripts/restart_timer.lua
+++ GoldenScripts/restart_timer.lua
@@ -1,7 +1,42 @@
--- Zone Complete Check
+-- Server Restart Timer Function
 
-local function zone_complete_check()
-	local red_zone_count = 0
+local function restartTimer(restartTime, restartHintTimeList, restartFlag, restartHint, restartHintLastsTime, deleteSave, savePath)
+    for key, value in pairs(restartHintTimeList) do -- Restart hint
+        mist.scheduleFunction(function()
+            trigger.action.outText(string.format(restartHint, value / 60), restartHintLastsTime)
+        end, {}, timer.getTime() + restartTime - value)
+    end
+
+    mist.scheduleFunction(function()
+        trigger.action.outText("服务器即将重启！", 180)
+    end, {}, timer.getTime() + restartTime - 15)
+
+    mist.scheduleFunction(function() -- Restart
+        if lfs and deleteSave == true and savePath ~= nil then
+            os.remove(lfs.writedir() .. "Missions/Saves/" .. savePath)
+        end
+
+        trigger.action.setUserFlag(restartFlag, true)
+    end, {}, timer.getTime() + restartTime + 15) -- Add 15s offset to avoid conflict with saving system
+end
+
+-- Server Restart Timer Function Done
+
+-- Scheduled Restart
+
+local scheduledRestartTime = 10800 -- 3 hours
+local scheduledRestartHintTimeList = { 60, 180, 300, 600, 900, 1200, 1500, 1800 }
+local scheduledRestartFlag = "FLAG_MISSION_RESTART"
+local scheduledRestartHint = "服务器将于 %d 分钟后定时重启！"
+
+restartTimer(scheduledRestartTime, scheduledRestartHintTimeList, scheduledRestartFlag, scheduledRestartHint, 90)
+
+-- Scheduled Restart Done
+
+-- Mission End Restart
+
+local function missionEndCondition()
+    local red_zone_count = 0
     local blue_zone_count = 0
 
 	for i, v in ipairs(bc:getZones()) do
@@ -19,60 +54,22 @@ local function zone_complete_check()
     return false
 end
 
--- Zone Complete Check Done
+local missionEndScheduler = nil
 
--- Server Restart Timer Function
-
-local function restartTimer(restartTime, restartHintTimeList, restartFlag, restartHint, restartHintLastsTime, deleteSave)
-    for key, value in pairs(restartHintTimeList) do -- Restart hint
-        mist.scheduleFunction(function(event, sender)
-            trigger.action.outText(string.format(restartHint, value / 60), restartHintLastsTime)
-        end, {}, timer.getTime() + restartTime - value)
-    end
-
-    mist.scheduleFunction(function(event, sender)
-        trigger.action.outText("服务器即将重启！", 180)
-    end, {}, timer.getTime() + restartTime - 15)
-
-    mist.scheduleFunction(function(event, sender) -- Restart
-        if deleteSave == true and lfs then
-            os.remove(lfs.writedir() .. "Missions/Saves/Caucasus-S-Saved-Data.lua")
-        end
-
-        trigger.action.setUserFlag(restartFlag, true)
-    end, {}, timer.getTime() + restartTime + 15) -- Add 15s offset to avoid conflict with saving system
-end
-
--- Server Restart Timer Function Done
-
--- Scheduled Restart
-
-local scheduledRestartTime = 10800 -- 3 hours
-local scheduledRestartHintTimeList = { 60, 180, 300, 600, 900, 1200, 1500, 1800 }
-local scheduledRestartFlag = "FLAG_MISSION_RESTART"
-local scheduledRestartHint = "服务器将于 %d 分钟后定时重启！"
-
-restartTimer(scheduledRestartTime, scheduledRestartHintTimeList, scheduledRestartFlag, scheduledRestartHint, 90, false)
-
--- Scheduled Restart Done
-
--- Mission Complete Restart
-
-local missionCompleteCheckScheduler = nil
-
-local missionCompleteCheck = function(event, sender)
-    if zone_complete_check() then
+local function missionEnd()
+    if missionEndCondition() then
         local restartTime = 60
         local restartHintTimeList = { 60 }
         local restartFlag = "FLAG_MISSION_RESTART"
-        local restartHint = "任务完成！服务器将于 %d 分钟后清档重启！"
+        local restartHint = "任务结束！服务器将于 %d 分钟后清档重启！"
+        local savePath = "Caucasus-S-Saved-Data.lua"
 
-        restartTimer(restartTime, restartHintTimeList, restartFlag, restartHint, 90, true)
+        restartTimer(restartTime, restartHintTimeList, restartFlag, restartHint, 90, true, savePath)
 
-        mist.removeFunction(missionCompleteCheckScheduler)
+        mist.removeFunction(missionEndScheduler)
     end
 end
 
-missionCompleteCheckScheduler = mist.scheduleFunction(missionCompleteCheck, {}, timer.getTime() + 90, 90)
+missionEndScheduler = mist.scheduleFunction(missionEnd, {}, timer.getTime() + 90, 90)
 
 -- Mission Complete Restart Done
