780db8a24f18fdc553a18a3446da5f0ba2e6249d
Gold John King <GoldJohnKing@Live.cn>
2023-11-17 17:01:07 +0800

    Bump mist to latest development branch 4.5.120 commit e6bdd50

--- GoldenMission/l10n/DEFAULT/Load_Scripts.lua
+++ GoldenMission/l10n/DEFAULT/Load_Scripts.lua
@@ -4,7 +4,7 @@ local script_list =
 {
     -- Load order must be correct
     "file_lib.lua",
-    "mist_4_5_113.lua",
+    "mist.lua",
     "GroupFunctions.lua",
     "GroundUnitAutoAttack.lua",
     "WeaponCooldown.lua",
