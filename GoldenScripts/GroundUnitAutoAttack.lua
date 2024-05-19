f33b17289a5a0e8e252aa705b82a1fedfa6c81a2
Gold John King <GoldJohnKing@Live.cn>
2024-05-19 18:02:31 +0800

    Use DCS Timer instead of mist in GroundUnitAutoAttack.lua

--- GoldenScripts/GroundUnitAutoAttack.lua
+++ GoldenScripts/GroundUnitAutoAttack.lua
@@ -2,12 +2,16 @@ local MoveToNearestEnemy = nil
 local IsEnemyLeaderUnitDead = nil
 
 function EnableAutoAttackForGroup(currentGroupName, searchRange, searchDuration, initialDelay)
-    return mist.scheduleFunction(MoveToNearestEnemy, { currentGroupName, searchRange, searchDuration }, timer.getTime() + initialDelay)
+    timer.scheduleFunction(MoveToNearestEnemy, { currentGroupName, searchRange, searchDuration }, timer.getTime() + initialDelay)
 end
 
-MoveToNearestEnemy = function(currentGroupName, searchRange, searchDuration)
+MoveToNearestEnemy = function(params)
+    local currentGroupName = params[1]
+    local searchRange = params[2]
+    local searchDuration = params[3]
+
     local currentGroup = Group.getByName(currentGroupName)
-    if currentGroup == nil or mist.groupIsDead(currentGroupName) == true then return end
+    if currentGroup == nil or mist.groupIsDead(currentGroupName) == true then return nil end
 
     local enemyCoalition = (currentGroup:getCoalition() == 1 and 2) or 1
     local enemyGroupList = coalition.getGroups(enemyCoalition, Group.Category.GROUND)
@@ -25,13 +29,13 @@ MoveToNearestEnemy = function(currentGroupName, searchRange, searchDuration)
         end
     end
 
-    if nearestEnemyGroupName == nil or mist.groupIsDead(nearestEnemyGroupName) == true then return end
+    if nearestEnemyGroupName == nil or mist.groupIsDead(nearestEnemyGroupName) == true then return nil end
 
     local enemyGroup = Group.getByName(nearestEnemyGroupName)
-    if enemyGroup == nil then return end
+    if enemyGroup == nil then return nil end
 
     local enemyLeaderUnit = enemyGroup:getUnit(1)
-    if enemyLeaderUnit == nil then return end
+    if enemyLeaderUnit == nil then return nil end
 
     local nearestEnemyGroupPosition = mist.getLeadPos(nearestEnemyGroupName)
     local path = {}
@@ -40,13 +44,19 @@ MoveToNearestEnemy = function(currentGroupName, searchRange, searchDuration)
 
     mist.goRoute(currentGroupName, path)
 
-    mist.scheduleFunction(IsEnemyLeaderUnitDead,
-        { enemyLeaderUnit, currentGroupName, nearestEnemyGroupName, searchRange, searchDuration },
-        timer.getTime() + searchDuration)
+    timer.scheduleFunction(IsEnemyLeaderUnitDead, { enemyLeaderUnit, currentGroupName, nearestEnemyGroupName, searchRange, searchDuration }, timer.getTime() + searchDuration)
+
+    return nil
 end
 
-IsEnemyLeaderUnitDead = function(enemyLeaderUnit, currentGroupName, nearestEnemyGroupName, searchRange, searchDuration)
-    if enemyLeaderUnit == nil or mist.groupIsDead(currentGroupName) == true then return end
+IsEnemyLeaderUnitDead = function(params)
+    local enemyLeaderUnit = params[1]
+    local currentGroupName = params[2]
+    local nearestEnemyGroupName = params[3]
+    local searchRange = params[4]
+    local searchDuration = params[5]
+
+    if enemyLeaderUnit == nil or mist.groupIsDead(currentGroupName) == true then return nil end
 
     if enemyLeaderUnit:isExist() == true then
         local currentGroupPosition = mist.getLeadPos(currentGroupName)
@@ -58,10 +68,10 @@ IsEnemyLeaderUnitDead = function(enemyLeaderUnit, currentGroupName, nearestEnemy
 
         mist.goRoute(currentGroupName, path)
 
-        mist.scheduleFunction(IsEnemyLeaderUnitDead,
-            { enemyLeaderUnit, currentGroupName, nearestEnemyGroupName, searchRange, searchDuration },
-            timer.getTime() + searchDuration)
+        timer.scheduleFunction(IsEnemyLeaderUnitDead, { enemyLeaderUnit, currentGroupName, nearestEnemyGroupName, searchRange, searchDuration }, timer.getTime() + searchDuration)
     else
         MoveToNearestEnemy(currentGroupName, searchRange, searchDuration)
     end
+    
+    return nil
 end
